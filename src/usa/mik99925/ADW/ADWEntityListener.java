94d8bd6430cf4add7e791a603cbe948af2baffbf
Mike <mik99925@gmail.com>
2011-04-09 17:37:23 -0700

    First commit.

---
+++ src/usa/mik99925/ADW/ADWEntityListener.java
@@ -0,0 +1,37 @@
+package usa.mik99925.ADW;
+
+import org.bukkit.entity.Entity;
+import org.bukkit.entity.Wolf;
+import org.bukkit.event.entity.EntityDamageByBlockEvent;
+import org.bukkit.event.entity.EntityDamageEvent;
+import org.bukkit.event.entity.EntityDamageEvent.DamageCause;
+import org.bukkit.event.entity.EntityListener;
+
+public class ADWEntityListener extends EntityListener {
+	public static ADW plugin;
+
+	public ADWEntityListener(ADW instance) {
+		plugin = instance;
+	}
+
+	public void onEntityDamageByBlock(EntityDamageByBlockEvent event) {
+		Entity defender = event.getEntity();
+		DamageCause type = event.getCause();
+
+		if (defender instanceof Wolf && type != DamageCause.VOID) {
+			event.setCancelled(true);
+			return;
+		}
+	}
+
+	public void onEntityDamage(EntityDamageEvent event) {
+		if (event.isCancelled()) {
+			return;
+		}
+
+		if (event instanceof EntityDamageByBlockEvent) {
+			this.onEntityDamageByBlock((EntityDamageByBlockEvent) event);
+			return;
+		}
+	}
+}
